8f51d982a8b4f7e4c0066d067bba9b5e60e357aa
Lynlxi <ib230408wai@setouchicollege.com>
2024-07-27 11:55:51 +0900

    “テキストP109まで完了”

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     /**
      * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
-        $tweets = Tweet::all();
-        
+        $tweets = $tweetService->getTweets();
         return view('tweet.index')
-            ->with('tweets', $tweets);    }
+            ->with('tweets', $tweets);
+    }
 }
